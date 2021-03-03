b7038806228d4df2bcab20848d130e4c7ac5f5ef
Sergei <mkezhman85@gmail.com>
2021-03-04 00:20:55 +0300

    [нов] First commit

---
+++ TodoApp/Controllers/UserController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TodoApp.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TodoApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+
+        readonly TodoAppContext _context;
+
+        public UserController(TodoAppContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("GetUsers")]
+        public IActionResult GetUsers()
+        {
+            var users = _context.user.ToList();
+            return Ok(users);
+        }
+
+        [HttpPost]
+        [Route("CreateUser")]
+        public JsonResult CreateUser([FromBody] User user)
+        {
+
+            _context.Add(user);
+            _context.SaveChanges();
+
+            return new JsonResult(user);
+        }
+
+        [HttpDelete("{id}")]
+        [Route("DeleteUserById")]
+        public async Task<IActionResult> DeleteUserById(int id)
+        {
+            var userItem = await _context.user.FindAsync(id);
+            if (userItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.user.Remove(userItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
+        [HttpGet("{id}")]
+        [Route("GetUserById")]
+        public async Task<ActionResult<User>> GetUserById(int id)
+        {
+            var userItem = await _context.user.FindAsync(id);
+
+            if (userItem == null)
+            {
+                return NotFound();
+            }
+
+            return userItem;
+        }
+
+
+    }
+}
